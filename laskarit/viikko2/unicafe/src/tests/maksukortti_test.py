50a5af8f8790a3d206fdc98758b909873f90980e
Jpentik <juha.ao.pentikainen@gmail.com>
2024-03-26 20:39:55 +0200

    Ennen testikattavuutta

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,29 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.maksukortti.lataa_rahaa(2500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 35.0)
+    
+    def test_saldo_vahenee_oikein(self):
+        self.maksukortti.ota_rahaa(100)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 9.0)
+
+    def test_saldo_sailyy_jos_raha_ei_riita(self):
+        self.maksukortti.ota_rahaa(11000)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+    
+    def test_arvo_frue_jos_saldo_riittaa(self):
+
+        self.assertTrue(self.maksukortti.ota_rahaa(100))
+
+    def test_arvo_false_jos_saldo_ei_riita(self):
+
+        self.assertFalse(self.maksukortti.ota_rahaa(1100))
